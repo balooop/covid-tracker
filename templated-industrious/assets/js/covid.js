6d8ea6b05ad562cb660c9470808481b7e36fd6f9
Eshan Dhar <eshandhar@Eshans-MacBook-Pro.local>
2020-10-31 20:40:26 -0500

    Basic first tab funcitonality

--- templated-industrious/assets/js/covid.js
+++ templated-industrious/assets/js/covid.js
@@ -1,5 +1,31 @@
-function initialize() {
-	alert("here");
-	var input = document.getElementById('searchTextField');
-	new google.maps.places.Autocomplete(input);
-  }
+function positive(){
+	var x = document.getElementById("posForm");
+	  x.style.display = "block";
+}
+
+function negative(){
+	var x = document.getElementById("posForm");
+	  x.style.display = "none";
+}
+
+function report(){
+	var x = document.getElementById("reportForm");
+	  x.style.display = "block";
+}
+function noreport(){
+	var x = document.getElementById("reportForm");
+		x.style.display = "none";
+}
+
+
+function finishSubmission(){
+	var x = document.getElementById("thankYou");
+	x.style.display = "block";
+
+	var x = document.getElementById("formInner");
+	x.style.display = "none";
+
+	// alert(document.getElementById("searchTextField0").value);
+	// alert(document.getElementById("searchTextField1").value);
+
+}
