2fa3f4e07c12fe620d265b73656c0ab99482b5c7
Pavan <pavan.baloo@gmail.com>
2020-11-06 03:56:55 -0600

    fuck school, all my homies hate school

--- mysql/insert_cases_trigger.sql
+++ mysql/insert_cases_trigger.sql
@@ -5,11 +5,11 @@ CREATE TRIGGER InsertAddressfromCases
     begin
         #### CREATE/UPDATE ADDRESSES TABLE ####
         # checks if address is in Addresses table via select
-        SET @count = (  SELECT _address 
+        SET @count = (  SELECT count(_address) 
                         FROM Addresses 
                         WHERE _address = new.address_visited);
         # if address not in Addresses table, insert the address
-        IF @count IS NULL THEN 
+        IF @count = 0 THEN 
             INSERT INTO Addresses(_address, block_id, num_cases) 
                         VALUES(new.address_visited, new.block_id, 1);
         # else, just update number of cases
@@ -20,6 +20,21 @@ CREATE TRIGGER InsertAddressfromCases
         END IF;
 
 
+        #### CREATE/UPDATE BLOCK TABLE ####
+        SET @block =  ( SELECT count(block_id)
+                        FROM Blocks
+                        WHERE new.block_id = block_id);
+        
+        IF @block = 0 THEN
+            INSERT INTO Blocks(block_id, num_cases_blk)
+                        VALUES(new.block_id, 1);
+        # if block does exist, update number of cases per block
+        ELSE
+            UPDATE Blocks
+            SET num_cases_blk = num_cases_blk + 1
+            WHERE block_id = new.block_id;
+
+        END IF;
 
     END;
     $$
@@ -27,7 +42,9 @@ CREATE TRIGGER InsertAddressfromCases
 
 
 
-/* DELIMITER $$
+/* 
+DEFINITELY WORKING
+DELIMITER $$
 CREATE TRIGGER InsertAddressfromCases
     AFTER INSERT ON Cases
     for each row
@@ -41,36 +58,9 @@ CREATE TRIGGER InsertAddressfromCases
         IF @count IS NULL THEN 
             INSERT INTO Addresses(_address, block_id, num_cases) 
                         VALUES(new.address_visited, new.block_id, 1);
-        
         # else, just update number of cases
         ELSE
             UPDATE Addresses
-            SET num_cases = (   SELECT count(*)
-                                FROM   Cases c
-                                WHERE  c._address = new.address_visited
-                            )
+            SET num_cases = num_cases + 1
             WHERE _address = new.address_visited;
-        END IF;
-
-
-        #### CREATE/UPDATE BLOCK TABLE ####
-        SET @block =  ( SELECT block_id
-                        FROM Blocks
-                        WHERE new.block_id = block_id);
-        # if block doesn't exist, insert into there
-        IF @block IS NULL THEN
-            INSERT INTO Blocks(block_id, num_cases_blk)
-                        VALUES(new.block_id, 1);
-        # if block does exist, update number of cases per block
-        ELSE
-            UPDATE Blocks
-            SET num_cases_blk = (SELECT count(num_cases)
-                                FROM Addresses
-                                GROUP BY block_id)
-            WHERE block_id = new.block_id;
-
-        END IF;
-
-    END;
-    $$
-    DELIMITER ; */
+        END IF; */
