f1fde62f470f2615591d1ed4399676ed2cf3e415
meetup2 <47257033+meetup2@users.noreply.github.com>
2020-11-06 01:24:52 -0600

    Update insert_cases_trigger.sql

--- mysql/insert_cases_trigger.sql
+++ mysql/insert_cases_trigger.sql
@@ -1,21 +1,20 @@
-CREATE TRIGGER InsertAddress
+DELIMITER $$
+CREATE TRIGGER InsertAddressfromCases
     AFTER INSERT ON Cases
     for each row
     begin
         SET @count = (  SELECT _address 
                         FROM Addresses 
-                        WHERE _address = new._address);
-        # insert case: if the address isn't present, we insert
+                        WHERE _address = new.address_visited);
         IF @count IS NULL THEN 
             INSERT INTO Addresses(_address, block_id, num_cases) 
-                        VALUES(new._address, new.block_id, 1);
-            # add to Blocks table
-        # update case: if the address is present, we update the num_cases
+                        VALUES(new.address_visited, new.block_id, 1);
         ELSE
             UPDATE Addresses
             SET num_cases = num_cases + 1
-            WHERE _address = new._address
+            WHERE _address = new.address_visited;
         END IF;
 
     END;
-
+    $$
+    DELIMITER ;
