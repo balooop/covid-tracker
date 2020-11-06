fef58c961d4ab8a142f1bbb38636779d27ec5fc4
Pavan <pavan.baloo@gmail.com>
2020-11-06 02:56:18 -0600

    reverted to working trigger- need to fix block update

--- mysql/insert_cases_trigger.sql
+++ mysql/insert_cases_trigger.sql
@@ -12,15 +12,37 @@ CREATE TRIGGER InsertAddressfromCases
         IF @count IS NULL THEN 
             INSERT INTO Addresses(_address, block_id, num_cases) 
                         VALUES(new.address_visited, new.block_id, 1);
-        
         # else, just update number of cases
         ELSE
             UPDATE Addresses
             SET num_cases = num_cases + 1
-                            #(   SELECT count(*)
-                            #    FROM   Cases c
-                            #    WHERE  c._address = new.address_visited
-                            #)
+            WHERE _address = new.address_visited;
+        END IF;
+
+
+
+/* DELIMITER $$
+CREATE TRIGGER InsertAddressfromCases
+    AFTER INSERT ON Cases
+    for each row
+    begin
+        #### CREATE/UPDATE ADDRESSES TABLE ####
+        # checks if address is in Addresses table via select
+        SET @count = (  SELECT _address 
+                        FROM Addresses 
+                        WHERE _address = new.address_visited);
+        # if address not in Addresses table, insert the address
+        IF @count IS NULL THEN 
+            INSERT INTO Addresses(_address, block_id, num_cases) 
+                        VALUES(new.address_visited, new.block_id, 1);
+        
+        # else, just update number of cases
+        ELSE
+            UPDATE Addresses
+            SET num_cases = (   SELECT count(*)
+                                FROM   Cases c
+                                WHERE  c._address = new.address_visited
+                            )
             WHERE _address = new.address_visited;
         END IF;
 
@@ -36,14 +58,13 @@ CREATE TRIGGER InsertAddressfromCases
         # if block does exist, update number of cases per block
         ELSE
             UPDATE Blocks
-            SET num_cases_blk = num_cases_blk + 1
-                                #(SELECT count(num_cases)
-                                #FROM Addresses
-                                #GROUP BY block_id)
+            SET num_cases_blk = (SELECT count(num_cases)
+                                FROM Addresses
+                                GROUP BY block_id)
             WHERE block_id = new.block_id;
 
         END IF;
 
     END;
     $$
-    DELIMITER ;
+    DELIMITER ; */
