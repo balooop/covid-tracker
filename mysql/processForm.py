0914220233af8dec7e92625ffe5ac5ffda9e56e0
Pavan <pavan.baloo@gmail.com>
2020-11-06 01:09:11 -0600

    updated process form lambda to only insert cases

--- mysql/processForm.py
+++ mysql/processForm.py
@@ -17,11 +17,10 @@ except pymysql.MySQLError as e:
     logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
     logger.error(e)
     sys.exit()
-
+    
 # logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 def handler(event, context):
     addCase(str(event['address']))
-    return "Added %d items from RDS MySQL table" %(item_count)
 
 
 def addCase(addresses_visited):
@@ -31,19 +30,18 @@ def addCase(addresses_visited):
     
     # connects to database
     with conn.cursor() as cur:
-        # inserts case into Cases
-        cur.execute('INSERT into Cases (address_visited, case_id, timestamp) Values ("'+addresses_visited+'", "'+case_id+'", CURRENT_TIMESTAMP)')
-        cur.execute("select * from Cases")
-        conn.commit()
-    
         # compute 'block_id'
         num_spaces = 0
         street_address = addresses_visited.split(',')[0].split(' ')
-        block_id = str((int)street_address[0]/100) + ''.join(street_address[1:])
-        
-        cur.execute(' INSERT into Addresses(_address, block_id) Values("'+addresses_visited+'", "'+block_id+'" ')
+        block_id = str((int(street_address[0])//100)*100) + ''.join(street_address[1:])
+
+
+        # inserts case into Cases
+        cur.execute('INSERT into Cases (address_visited, case_id, timestamp, block_id) Values ("'+addresses_visited+'", "'+case_id+'", CURRENT_TIMESTAMP, "'+block_id+'")')
         conn.commit()
-    conn.commit()
+    
+        
+        conn.commit()
     return
     
 # parse data from website (addresses visited + businesses reported)
