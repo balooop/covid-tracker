98356dba05b8f5766ca13e0df996dadb234f04a6
meetup2 <47257033+meetup2@users.noreply.github.com>
2020-11-05 23:03:45 -0600

    Update covid_locations_db_init.sql
    update

--- mysql/covid_locations_db_init.sql
+++ mysql/covid_locations_db_init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Cases(
     address_visited char(100) NOT NULL,
-    case_id INT NOT NULL,
+    case_id VARCHAR(255) NOT NULL,
     timestamp DATETIME,
     PRIMARY KEY(case_id, address_visited)
 );
 CREATE TABLE Addresses(
     _address VARCHAR(255) NOT NULL,
-    block_id INT,
+    block_id VARCHAR(255),
     num_cases INT,
     PRIMARY KEY(_address)
 );
