e6f89dfbd4339971c3ce41bab04b752d1a0a2c81
Pavan <pavan.baloo@gmail.com>
2020-11-05 20:41:49 -0600

    moved js to covid.js

--- templated-industrious/assets/js/covid.js
+++ templated-industrious/assets/js/covid.js
@@ -1,3 +1,14 @@
+// Configure AWS SDK for JavaScript & set region and credentials
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-2'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+	IdentityPoolId: 'us-east-2:fad8bf8b-3e89-436a-8120-f35b1c8c3d7e',
+});
+AWS.config.apiVersions = {
+	lambda: '2015-03-31',
+	// other service API versions
+};
+
 function positive(){
 	var x = document.getElementById("posForm");
 	  x.style.display = "block";
@@ -17,6 +28,20 @@ function noreport(){
 		x.style.display = "none";
 }
 
+// called when 
+function processForm() {
+	var lambda = new AWS.Lambda({region: 'us-east-2', apiVersion: '2015-03-31'});
+	var params = {
+		FunctionName: 'arn:aws:lambda:us-east-2:834423887668:function:submit',
+		InvocationType: 'RequestResponse',
+		Payload: JSON.stringify({"address": document.getElementById("searchTextField0").value})
+	};
+	lambda.invoke(params, function(err,data){
+		if (err) 	alert("err,err.stack");
+		else		alert("success!");
+	});
+	finishSubmission(); 
+};
 
 function finishSubmission(){
 	var x = document.getElementById("thankYou");
